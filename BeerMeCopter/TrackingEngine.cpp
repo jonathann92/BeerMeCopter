7e63d8b66de10e5e64e383781d2387e5c37cc7bf
James Griffin <jegriffi@uci.edu>
2015-08-18 20:34:08 -0700

    formatted provided code

--- BeerMeCopter/TrackingEngine.cpp
+++ BeerMeCopter/TrackingEngine.cpp
@@ -45,23 +45,14 @@ const string trackbarWindowName = "Trackbars";
 void on_trackbar(int, void*)
 {//This function gets called whenever a
 	// trackbar position is changed
-
-
-
-
-
 }
 string intToString(int number){
-
-
 	std::stringstream ss;
 	ss << number;
 	return ss.str();
 }
 void createTrackbars(){
 	//create window for trackbars
-
-
 	namedWindow(trackbarWindowName, 0);
 	//create memory to store trackbar name on window
 	char TrackbarName[50];
@@ -82,31 +73,27 @@ void createTrackbars(){
 	createTrackbar("S_MAX", trackbarWindowName, &S_MAX, S_MAX, on_trackbar);
 	createTrackbar("V_MIN", trackbarWindowName, &V_MIN, V_MAX, on_trackbar);
 	createTrackbar("V_MAX", trackbarWindowName, &V_MAX, V_MAX, on_trackbar);
-
-
 }
 void drawObject(int x, int y, Mat &frame){
-
-	//use some of the openCV drawing functions to draw crosshairs
-	//on your tracked image!
-
-	//UPDATE:JUNE 18TH, 2013
 	//added 'if' and 'else' statements to prevent
 	//memory errors from writing off the screen (ie. (-25,-25) is not within the window!)
-
 	circle(frame, Point(x, y), 20, Scalar(0, 255, 0), 2);
 	if (y - 25>0)
 		line(frame, Point(x, y), Point(x, y - 25), Scalar(0, 255, 0), 2);
-	else line(frame, Point(x, y), Point(x, 0), Scalar(0, 255, 0), 2);
+	else 
+		line(frame, Point(x, y), Point(x, 0), Scalar(0, 255, 0), 2);
 	if (y + 25<FRAME_HEIGHT)
 		line(frame, Point(x, y), Point(x, y + 25), Scalar(0, 255, 0), 2);
-	else line(frame, Point(x, y), Point(x, FRAME_HEIGHT), Scalar(0, 255, 0), 2);
+	else 
+		line(frame, Point(x, y), Point(x, FRAME_HEIGHT), Scalar(0, 255, 0), 2);
 	if (x - 25>0)
 		line(frame, Point(x, y), Point(x - 25, y), Scalar(0, 255, 0), 2);
-	else line(frame, Point(x, y), Point(0, y), Scalar(0, 255, 0), 2);
+	else 
+		line(frame, Point(x, y), Point(0, y), Scalar(0, 255, 0), 2);
 	if (x + 25<FRAME_WIDTH)
 		line(frame, Point(x, y), Point(x + 25, y), Scalar(0, 255, 0), 2);
-	else line(frame, Point(x, y), Point(FRAME_WIDTH, y), Scalar(0, 255, 0), 2);
+	else 
+		line(frame, Point(x, y), Point(FRAME_WIDTH, y), Scalar(0, 255, 0), 2);
 
 	putText(frame, intToString(x) + "," + intToString(y), Point(x, y + 30), 1, 1, Scalar(0, 255, 0), 2);
 
@@ -114,24 +101,19 @@ void drawObject(int x, int y, Mat &frame){
 void morphOps(Mat &thresh){
 
 	//create structuring element that will be used to "dilate" and "erode" image.
-	//the element chosen here is a 3px by 3px rectangle
-
+	//erodeElement is of size 12x12 to get rid of superfluous noise and make object tracking cleaner
 	Mat erodeElement = getStructuringElement(MORPH_RECT, Size(12, 12));
+
 	//dilate with larger element so make sure object is nicely visible
 	Mat dilateElement = getStructuringElement(MORPH_RECT, Size(8, 8));
 
 	erode(thresh, thresh, erodeElement);
 	erode(thresh, thresh, erodeElement);
 
-
 	dilate(thresh, thresh, dilateElement);
 	dilate(thresh, thresh, dilateElement);
-
-
-
 }
 void trackFilteredObject(int &x, int &y, Mat threshold, Mat &cameraFeed){
-
 	Mat temp;
 	threshold.copyTo(temp);
 	//these two vectors needed for output of findContours
@@ -147,7 +129,6 @@ void trackFilteredObject(int &x, int &y, Mat threshold, Mat &cameraFeed){
 		//if number of objects greater than MAX_NUM_OBJECTS we have a noisy filter
 		if (numObjects<MAX_NUM_OBJECTS){
 			for (int index = 0; index >= 0; index = hierarchy[index][0]) {
-
 				Moments moment = moments((cv::Mat)contours[index]);
 				double area = moment.m00;
 
@@ -161,9 +142,9 @@ void trackFilteredObject(int &x, int &y, Mat threshold, Mat &cameraFeed){
 					objectFound = true;
 					refArea = area;
 				}
-				else objectFound = false;
-
-
+				else {
+					objectFound = false;
+				}
 			}
 			//let user know you found an object
 			if (objectFound){
@@ -172,33 +153,35 @@ void trackFilteredObject(int &x, int &y, Mat threshold, Mat &cameraFeed){
 				//draw object location on screen
 				drawObject(x, y, cameraFeed);
 			}
-
-		}
-		else {
-			putText(cameraFeed, "TOO MUCH NOISE! ADJUST FILTER", Point(0, 50), 1, 2, Scalar(0, 0, 255), 2);
+			else {
+				putText(cameraFeed, "TOO MUCH NOISE! ADJUST FILTER", Point(0, 50), 1, 2, Scalar(0, 0, 255), 2);
+			}
 		}
 	}
 }
 int main(int argc, char* argv[])
 {
-	//some boolean variables for different functionality within this
-	//program
-	bool trackObjects = true;
-	bool useMorphOps = true;
 	//Matrix to store each frame of the webcam feed
 	Mat cameraFeed;
+	
 	//matrix storage for HSV image
 	Mat HSV;
+	
 	//matrix storage for binary threshold image
 	Mat threshold;
+	
 	//x and y values for the location of the object
 	int x = 0, y = 0;
+	
 	//create slider bars for HSV filtering
 	createTrackbars();
+	
 	//video capture object to acquire webcam feed
 	VideoCapture capture;
+	
 	//open capture object at location zero (default location for webcam)
 	capture.open(0);
+	
 	//set height and width of capture frame
 	capture.set(CV_CAP_PROP_FRAME_WIDTH, FRAME_WIDTH);
 	capture.set(CV_CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT);
@@ -207,36 +190,31 @@ int main(int argc, char* argv[])
 	while (1){
 		//store image to matrix
 		capture.read(cameraFeed);
+		
 		//convert frame from BGR to HSV colorspace
 		cvtColor(cameraFeed, HSV, COLOR_BGR2HSV);
+		
 		//filter HSV image between values and store filtered image to
 		//threshold matrix
 		inRange(HSV, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), threshold);
+
 		//perform morphological operations on thresholded image to eliminate noise
 		//and emphasize the filtered object(s)
-		if (useMorphOps)
-			morphOps(threshold);
+		morphOps(threshold);
+
 		//pass in thresholded frame to our object tracking function
 		//this function will return the x and y coordinates of the
 		//filtered object
-		if (trackObjects)
-			trackFilteredObject(x, y, threshold, cameraFeed);
+		trackFilteredObject(x, y, threshold, cameraFeed);
 
 		//show frames 
 		imshow(windowName2, threshold);
 		imshow(windowName, cameraFeed);
 		imshow(windowName1, HSV);
 
-
 		//delay 30ms so that screen can refresh.
 		//image will not appear without this waitKey() command
 		waitKey(30);
 	}
-
-
-
-
-
-
 	return 0;
 }
