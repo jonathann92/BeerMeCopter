d0ed2629313425a522cd7837f108455aec82a3c9
Jonathan Nguyen <Jonathann92@gmail.com>
2015-09-01 01:50:13 -0700

    Compiled on my mac using terminal using opencv and cmake. I changed the headers for opencv, the path for windows uses \ slashes and linux uses / slashes.

---
+++ BeerMeCopter/CMakeFiles/TrackingEngine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/TrackingEngine.dir/TrackingEngine.cpp.o"
+  "TrackingEngine.pdb"
+  "TrackingEngine"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/TrackingEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- BeerMeCopter/TrackingEngine.cpp
+++ BeerMeCopter/TrackingEngine.cpp
@@ -1,8 +1,14 @@
 #include <sstream>
 #include <string>
 #include <iostream>
-#include <opencv\highgui.h>
-#include <opencv\cv.h>
+
+/* For Windows */
+//#include <opencv\highgui.h>
+//#include <opencv\cv.h>
+
+/* For Linux */
+#include <opencv/highgui.h>
+#include <opencv/cv.h>
 
 using namespace cv;
 //initial min and max HSV filter values.
